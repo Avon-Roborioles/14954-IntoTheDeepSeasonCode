4e7150c93a5bdbc4aa661d4a6db23e754e8b2ee3
ayoda1122 <aidan.j.yoder@gmail.com>
2024-09-30 07:15:23 -0400

    Pathing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoNoMoose/PedroAutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoNoMoose/PedroAutoTest.java
@@ -1,22 +1,36 @@
 package org.firstinspires.ftc.teamcode.AutoNoMoose;
 
+
+
+import static java.lang.Math.PI;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+import org.firstinspires.ftc.teamcode.pedroPathing.follower.Follower;
 import org.firstinspires.ftc.teamcode.pedroPathing.localization.Pose;
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.BezierCurve;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Path;
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Point;
 
 @Autonomous(name="PedroAutoTest", group="Auto Tests")
 public class PedroAutoTest extends AutoBase{
-    private Path path1;
-    private Path path2;
-    private Path path3;
-
-    public Pose pose1 = new Pose(20, 20, 0);
-
-    private Pose startPose = new Pose(0, 0, 0);
     @Override
     public void runOpMode() throws InterruptedException {
-        initializeAuto();
+
+        initializeAuto(telemetry, hardwareMap);
+
+        follower = new Follower(hardwareMap);
+        path1 = new Path(new BezierCurve(new Point(startPose), new Point(pose1), new Point(new Pose(40,-20, PI))));
+        path1.setConstantHeadingInterpolation(0);
+        path1.setPathEndTimeoutConstraint(6);
+        follower.setPose(startPose);
+        waitForStart();
+        follower.followPath(path1);
+        while (follower.isBusy() && !isStopRequested()){
+            follower.telemetryDebug(mTelemetry);
+            follower.update();
+        }
+
 
     }
 
